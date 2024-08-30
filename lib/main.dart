4ce53a3892bf069e83dde290432d57fbfea9f980
AadhithyaVenkatesan <aadhithyavenkatesan1@gmail.com>
2024-08-30 09:40:00 +0530

    Semi-Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.6852);
+  void _onMapCreated(GoogleMapController controller){
+    mapController = controller;
+  }
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],   
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+         
         ),
       ),
     );
---
+++ lib/src/locations.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'locations.g.dart';
+
+@JsonSerializable()
+class LatLng {
+  LatLng({
+    required this.lat,
+    required this.lng,
+  });
+
+
+  factory LatLng.fromJson(Map<String, dynamic> json) => _$LatLngFromJson(json);
+  Map<String, dynamic> toJson() => _$LatLngToJson(this);
+
+  final double lat;
+  final double lng;
+}
+
+
+@jsonSerializable()
+class Region {
+  Region({
+    required this.coords,
+    required this.id,
+    required this.name,
+    required this.zoom,
+  });
+
+  factory Region.fromJson(Map<String, dynamic> json) => _$RegionFromJson(json);
+  Map<String, dynamic> toJson() => _$RegionToJson(this);
+
+  final LatLng coords;
+  final String id;
+  final String name;
+  final double zoom;
+}
